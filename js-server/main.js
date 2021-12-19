aed16eb75de9357c893e365b6512a26fcf80a73e
Tim <nebukam@gmail.com>
2021-12-19 02:16:42 -0500

    Update

--- js-server/main.js
+++ js-server/main.js
@@ -1,31 +1,35 @@
 const server = require(`@nkmjs/core/server`);
 const handlers = require(`./handlers`);
-class ServerProcess extends server.core.ServerBase{
-    constructor(p_config){super(p_config);}
+class ServerProcess extends server.core.ServerBase {
+    constructor(p_config) { super(p_config); }
 
-    _Init(){
-        
+    _Init() {
+
         super._Init();
 
         this._RegisterAPIs({
-            getUserProfile:{
-                route:`/user/profile/:id`,
-                handler:handlers.UserProfile
+            getUserProfile: {
+                route: `/user/profile/:id`,
+                handler: handlers.UserProfile,
+                start: true
             },
-            getUserProfile64:{
-                route:`/user/profile64/:id`,
-                handler:handlers.UserProfile64
+            getUserProfile64: {
+                route: `/user/profile64/:id`,
+                handler: handlers.UserProfile64,
+                start: true
             },
-            getFriendlist:{
-                route:`/user/friendlist/:id`,
-                handler:handlers.UserFriendlist
+            getFriendlist: {
+                route: `/user/friendlist/:id`,
+                handler: handlers.UserFriendlist,
+                start: true
             },
-            getLibrary:{
-                route:`/user/library/:id`,
-                handler:handlers.UserLibrary
+            getLibrary: {
+                route: `/user/library/:id`,
+                handler: handlers.UserLibrary,
+                start: true
             }
         });
-        
+
     }
 
 }
